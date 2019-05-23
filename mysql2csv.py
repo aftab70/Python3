f98ad6f11e93d0974c7fc88dd065e1dbfb56e34e
aftab70 <43289912+aftab70@users.noreply.github.com>
2019-05-24 01:37:58 +0530

    Update mysql2csv.py

--- mysql2csv.py
+++ mysql2csv.py
@@ -5,9 +5,9 @@ import sys
 import csv
 import os
 
-db = pymysql.connect("localhost","admin","123456","aftab" )
+db = pymysql.connect("localhost","username","password","database_name" )
 cur = db.cursor()
-sql = 'select * from EMPLOYEE;'
+sql = 'select * from Table_name;'
 cur.execute(sql)
 
 rows = cur.fetchall()
@@ -16,8 +16,4 @@ myFile = csv.writer(fp)
 myFile.writerows(rows)
 fp.close()
 db.close()
-os.system("""curl 'http://localhost:8983/solr/user_core/update/csv?commit=true' --data-binary @/tmp/file.csv -H 'Content-type:text/plain; charset=utf-8'""")
-
-
-
-
+os.system("""curl 'http://localhost:8983/solr/core_name/update/csv?commit=true' --data-binary @path_of_the_file.csv -H 'Content-type:text/plain; charset=utf-8'""")
