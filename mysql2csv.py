f269ee90c4720bbb586d317ec52411d4af45bcaa
aftab70 <43289912+aftab70@users.noreply.github.com>
2019-05-24 01:19:31 +0530

    Update mysql2csv.py

--- mysql2csv.py
+++ mysql2csv.py
@@ -15,3 +15,9 @@ myFile = csv.writer(fp)
 myFile.writerows(rows)
 fp.close()
 db.close()
+
+import os
+cmd = "'curl 'http://localhost:8983/solr/user_core/update/csv?commit=true' --data-binary @file.csv -H 'Content-type:text/plain; charset=utf-8'"
+os.system(cmd)
+
+
