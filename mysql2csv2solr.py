a21dd28833dc6a57fa74723f8c3edf5ef5101234
aftab70 <43289912+aftab70@users.noreply.github.com>
2019-05-24 19:42:12 +0530

    Update mysql2csv2solr.py

--- mysql2csv2solr.py
+++ mysql2csv2solr.py
@@ -5,7 +5,7 @@ import sys
 import csv
 import os
 
-db = pymysql.connect("localhost","admin","123456","aftab" )
+db = pymysql.connect("localhost","user_name","password","Databases" )
 cur = db.cursor()
 sql = "select ID,CONCAT(LastName,' ',FirstName) FROM Persons;"
 cur.execute(sql)
@@ -18,4 +18,4 @@ myFile.writerows(rows)
 
 fp.close()
 db.close()
-os.system("""curl -u admin:123456 'http://localhost:8983/solr/user_core/update/csv?commit=true' --data-binary @/tmp/file.csv -H 'Content-type:text/plain; charset=utf-8'""")
+os.system("""curl -u user:password 'http://localhost:8983/solr/user_core/update/csv?commit=true' --data-binary @/tmp/file.csv -H 'Content-type:text/plain; charset=utf-8'""")
