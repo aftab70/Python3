138c34f600f416e2e1bc57aac848aadc7b6d1bf5
aftab70 <43289912+aftab70@users.noreply.github.com>
2020-03-03 13:15:01 +0530

    Update mysql2csv2solr.py

--- mysql2csv2solr.py
+++ mysql2csv2solr.py
@@ -19,3 +19,33 @@ myFile.writerows(rows)
 fp.close()
 db.close()
 os.system("""curl -u user:password 'http://localhost:8983/solr/user_core/update/csv?commit=true' --data-binary @/tmp/file.csv -H 'Content-type:text/plain; charset=utf-8'""")
+
+
+#################################################################################################################################################################################
+
+#!/usr/bin/python3
+
+import pymysql
+import sys
+import csv
+import os
+
+db = pymysql.connect("IP","username","password","database_name" )
+cur = db.cursor()
+sql = 'SELECT * FROM MOVIES;'
+cur.execute(sql)
+
+rows = cur.fetchall()
+
+f = open('/tmp/file.csv', 'w')
+myFile = csv.writer(f)
+myFile.writerow(['ID','Movies','Director'])
+myFile.writerows(rows)
+
+f.close()
+db.close()
+
+os.system("""curl -u admin:PASSWORD 'http://localhost:8983/solr/movies_core/update/csv?commit=true' --data-binary @/tmp/file.csv -H 'Content-type:text/plain; charset=utf-8'""")
+
+
+######################################################################################################################################################################################
